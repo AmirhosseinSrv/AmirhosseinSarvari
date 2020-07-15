e513f9b530be2987c4135a5593f2fc7b11284c5d
stars1378 <amirhossein.srv@gmail.com>
2020-07-15 19:57:54 +0430

    Clinet side

---
+++ precache-manifest.bfde35a650ae2b4c6d33e9308f9131e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daa2316856ef01431a199f4ee97e272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace911b80f93f47622f2",
+    "url": "/static/css/2.b42c401b.chunk.css"
+  },
+  {
+    "revision": "0d3efe43f1bf036e9edd",
+    "url": "/static/css/main.0887b26e.chunk.css"
+  },
+  {
+    "revision": "ace911b80f93f47622f2",
+    "url": "/static/js/2.2a14b2ff.chunk.js"
+  },
+  {
+    "revision": "5d02016f6a9a3fbb9bc638d50f0a8b90",
+    "url": "/static/js/2.2a14b2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3efe43f1bf036e9edd",
+    "url": "/static/js/main.2f2affe1.chunk.js"
+  },
+  {
+    "revision": "0a94005665a4648de920",
+    "url": "/static/js/runtime-main.f775f7b9.js"
+  }
+]);
